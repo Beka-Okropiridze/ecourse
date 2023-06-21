a4d34fc48bcf135bdc85d4114f0dc4400dc1e2c8
beqa <beqa.oqropiridze1994@gmail.com>
2023-06-21 17:38:38 +0400

    registration page added

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,14 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage, ProductList } from '../pages';
 import { ProductDetail } from '../pages';
+import { Register } from '../pages';
+import { Login } from '../pages';
 
 export const AllRoutes = () => {
   return (
     <>
      <Routes>
-        <Route path='' element={<HomePage title='eBook App' />} />
-        <Route path='/products' element={<ProductList title='All eBooks' />} />
-        <Route path='/products/:id' element={<ProductDetail title='About eBook' />} />
+        <Route path='/' element={<HomePage title='eBook App' />} />
+        <Route path='products' element={<ProductList title='All eBooks' />} />
+        <Route path='products/:id' element={<ProductDetail title='About eBook' />} />
+
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
      </Routes>
     </>
   )
